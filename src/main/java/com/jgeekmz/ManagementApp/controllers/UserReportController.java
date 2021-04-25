e16da28ac476eed0299e435fa4ea436b258ddf98
jgeekmz <60450637+jgeekmz@users.noreply.github.com>
2021-04-25 14:20:17 +0200

    25.4.2021- Merge Try

--- src/main/java/com/jgeekmz/ManagementApp/controllers/UserReportController.java
+++ src/main/java/com/jgeekmz/ManagementApp/controllers/UserReportController.java
@@ -1,6 +1,5 @@
 package com.jgeekmz.ManagementApp.controllers;
 
-import com.jgeekmz.ManagementApp.models.User;
 import com.jgeekmz.ManagementApp.security.ActiveUserStore;
 import com.jgeekmz.ManagementApp.services.UserService;
 import org.slf4j.Logger;
@@ -9,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Locale;
 
@@ -23,20 +19,10 @@ public class UserReportController {
     @Autowired
     UserService userService;
 
-    //Track of logged users
+    // Track of logged users
     @Autowired
     ActiveUserStore activeUserStore;
 
-    /*@RequestMapping(value="/usersReports", method = RequestMethod.GET)
-    public ModelAndView showUserReports (ModelAndView modelAndView, User user, Locale locale) {
-
-        //When the user was registered, on which date
-        //List<User> userList = userService.findAll();
-
-        modelAndView.addObject("users", activeUserStore.getUsers());
-        modelAndView.setViewName("usersReport");
-        return modelAndView;
-    }*/
 
     @GetMapping("/usersReports")
     public String getLoggedUsers(Locale locale, Model model) {
@@ -46,6 +32,5 @@ public class UserReportController {
         model.addAttribute("msg", "Welcome to the Admin Report Page!");
         return "usersReport";
     }
-
 
 }
